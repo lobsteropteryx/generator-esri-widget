4d731dc15dc8d6ab7295697a68147056efc6a288
lobsteropteryx <ian.firkin@gmail.com>
2015-04-25 23:41:12 -0700

    Fixing template for tests

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
                     template: 'tests/esri_amd.html',
                     keepRunner: true,
                     outfile: 'tests/_SpecRunner.html',
+                    host: 'http://127.0.0.1:8000/',
                     vendor: []
                 }
             }
